709efa6219056668dfabc3e721001c329e2e04d6
josh-W42 <wilson.jd022@gmail.com>
2024-12-01 09:09:23 -0800

    day 1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,8 +1,6 @@
 """
 Historian Hysteria
 """
-
-
 import os
 
 current_dir = os.path.join(os.getcwd(), 'day_1')
@@ -11,7 +9,6 @@ data_path = os.path.join(current_dir, 'input.txt')
 """
 Data is n = 1000
 """
-
 
 
 """
@@ -32,6 +29,32 @@ def part_one_solution(l1: list[int], l2: list[int]) -> int:
     return result
 
 
+"""
+Goal:
+Find a total similarity score.
+O(n) time O(n) space solution
+Use a dictionary, count the frequency of each value of l2.
+Loop through l1:
+    if in dictionary:
+        add similarity score to result
+"""
+def part_two_solution(l1: list[int], l2: list[int]) -> int:
+    result = 0
+
+    freq = {}
+    for val in l2:
+        if val in freq:
+            freq[val] += 1
+        else:
+            freq[val] = 1
+
+    for val in l1:
+        if val in freq:
+            result += freq[val] * val
+
+    return result
+
+
 def parse_data() -> [list[int], list[int]]:
     col_1 = []
     col_2 = []
@@ -41,10 +64,10 @@ def parse_data() -> [list[int], list[int]]:
             col_1.append(int(result[0]))
             col_2.append(int(result[-1]))
 
-
     return [col_1, col_2]
 
 
 if __name__ == '__main__':
     [a, b] = parse_data()
     part_one = part_one_solution(a, b)
+    part_two = part_two_solution(a, b)
