a74c6015b0b97e2cf9f603c68e6a91b5431af611
josh-W42 <wilson.jd022@gmail.com>
2024-12-02 11:47:37 -0800

    day 2 part 2

--- day_2/main.py
+++ day_2/main.py
@@ -58,8 +58,61 @@ def part_one_solution(data: list[list[int]]) -> int:
     return result
 
 
+def is_row_safe(row: list[int], is_increasing):
+    for i in range(len(row) - 1):
+        if row[i] > row[i + 1] and is_increasing:
+            return False
+
+        if row[i] < row[i + 1] and not is_increasing:
+            return False
+
+        diff = abs(row[i] - row[i + 1])
+        if diff < 1 or diff > 3:
+            return False
+
+    return True
+
+
+def part_two_solution(data: list[list[int]]) -> int:
+    """
+    Count the number of 'safe' rows in the data.
+
+    A row is 'safe' if:
+        - It is strictly increasing or decreasing
+        - The difference between any two adjacent levels is in the range [1, 3].
+
+    Similar to before, but now if an unsafe row becomes safe after removing one
+    item, then that row should be counted as well.
+
+    I define an unsafe value as a value that causes the row to become unsafe.
+
+    Because input side is small (10^3) this is ok but for larger inputs (>= 10^4), this solution would be naive.
+
+    :param data: input data
+    :return: The number of safe rows.
+    """
+    result = 0
+    for row in data:
+
+        # Is the unsafe value in the first or last positions?
+        # Also, if there isn't an unsafe value at all we end here.
+        if is_row_safe(row[1:], row[1] < row[2]) or is_row_safe(row[:len(row) - 1], row[0] < row[1]):
+            result += 1
+            continue
+
+        for i in range(1, len(row) - 1):
+            left = row[:i]
+            right = row[i + 1:]
+
+            # is the unsafe value at position i?
+            if is_row_safe(left + right, row[i - 1] < row[i + 1]):
+                result += 1
+                break
+
+    return result
+
+
 if __name__ == '__main__':
     results = parse_data()
     part_one = part_one_solution(results)
-
-    print(part_one)
+    part_two = part_two_solution(results)
