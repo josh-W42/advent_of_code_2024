8be0ade42fba567a4b16c8464032bbdda8660b2b
josh-W42 <wilson.jd022@gmail.com>
2024-12-12 16:52:08 -0800

    day 12 part 2

--- day_12/main.py
+++ day_12/main.py
@@ -3,6 +3,7 @@ Garden Groups
 """
 
 import os
+from functools import cmp_to_key
 
 input_path = os.path.join(os.curdir, 'input.txt')
 
@@ -111,6 +112,169 @@ def part_one_solution(mat: list[list[str]]) -> int:
     return result
 
 
+def compare_x(a, b):
+    if a[1] == b[1]:
+        return a[0] - b[0]
+
+    return a[1] - b[1]
+
+
+def compare_y(a, b):
+    if a[0] == b[0]:
+        return a[1] - b[1]
+
+    return a[0] - b[0]
+
+
+def part_two_solution(mat: list[list[str]]) -> int:
+    """
+    Similar to before, only now we calculate the perimeter by finding the
+    number of sides for the region.
+
+    So we take the points on the edges and sort them by they're respective directions in ascending order
+    and look at them in pairs. if they're right next to each other, they only are counted as a single segment otherwise
+    they're counted as two segments.
+
+    :param mat:
+    :return:
+    """
+
+    result = 0
+
+    # so we do not recalculate regions we've already traveled
+    visited_regions = set()
+
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+
+            if (i, j) in visited_regions:
+                continue
+
+            stack = [(i, j)]
+            origin = mat[i][j]
+            visited_regions.add((i, j))
+
+            # So points that we've visited do not re-enter the stack.
+            # but we still recognize other regions as foreign, even if we've
+            # already traversed them.
+            inner_visited = {(i, j)}
+
+            # The area in this case can be counted by counting all
+            # the blocks that have the origin letter
+            area = 0
+
+            sides = {'+y': [], '-y': [], '+x': [], '-x': []}
+            while stack:
+                node = stack.pop()
+                a, b = node
+                area += 1
+
+                if a == 0 or a == len(mat) - 1:
+                    if a == 0:
+                        sides['-y'].append((-1, b))
+
+                    else:
+                        sides['+y'].append((len(mat), b))
+
+                if b == 0 or b == len(mat[0]) - 1:
+                    if b == 0:
+                        sides['-x'].append((a, -1))
+
+                    else:
+                        sides['+x'].append((a, len(mat[0])))
+
+                if a - 1 >= 0 and (a - 1, b) not in inner_visited:
+
+                    if mat[a - 1][b] == origin:
+                        visited_regions.add((a - 1, b))
+                        stack.append((a - 1, b))
+                        inner_visited.add((a - 1, b))
+                    else:
+                        sides['-y'].append((a - 1, b))
+
+                if a + 1 < len(mat) and (a + 1, b) not in inner_visited:
+
+                    if mat[a + 1][b] == origin:
+                        visited_regions.add((a + 1, b))
+                        stack.append((a + 1, b))
+                        inner_visited.add((a + 1, b))
+                    else:
+                        sides['+y'].append((a + 1, b))
+
+                if b - 1 >= 0 and (a, b - 1) not in inner_visited:
+
+                    if mat[a][b - 1] == origin:
+                        visited_regions.add((a, b - 1))
+                        stack.append((a, b - 1))
+                        inner_visited.add((a, b - 1))
+                    else:
+                        sides['-x'].append((a, b - 1))
+
+                if b + 1 < len(mat[0]) and (a, b + 1) not in inner_visited:
+
+                    if mat[a][b + 1] == origin:
+                        visited_regions.add((a, b + 1))
+                        stack.append((a, b + 1))
+                        inner_visited.add((a, b + 1))
+                    else:
+                        sides['+x'].append((a, b + 1))
+
+            # Begin segment side counting.
+            sides['+x'].sort(key=cmp_to_key(compare_x))
+            right_side = 0 if len(sides['+x']) == 0 else 1
+
+            for j in range(len(sides['+x']) - 1):
+                y_1, x_1 = sides['+x'][j]
+                y_2, x_2 = sides['+x'][j + 1]
+
+                if x_1 == x_2 and y_1 + 1 != y_2:
+                    right_side += 1
+                elif x_1 != x_2:
+                    right_side += 1
+
+            sides['-x'].sort(key=cmp_to_key(compare_x))
+            left_side = 0 if len(sides['-x']) == 0 else 1
+
+            for j in range(len(sides['-x']) - 1):
+                y_1, x_1 = sides['-x'][j]
+                y_2, x_2 = sides['-x'][j + 1]
+
+                if x_1 == x_2 and y_1 + 1 != y_2:
+                    left_side += 1
+                elif x_1 != x_2:
+                    left_side += 1
+
+            sides['+y'].sort(key=cmp_to_key(compare_y))
+            up_side = 0 if len(sides['+y']) == 0 else 1
+
+            for j in range(len(sides['+y']) - 1):
+                y_1, x_1 = sides['+y'][j]
+                y_2, x_2 = sides['+y'][j + 1]
+
+                if y_1 == y_2 and x_1 + 1 != x_2:
+                    up_side += 1
+                elif y_1 != y_2:
+                    up_side += 1
+
+            sides['-y'].sort(key=cmp_to_key(compare_y))
+            down_side = 0 if len(sides['-y']) == 0 else 1
+
+            for j in range(len(sides['-y']) - 1):
+                y_1, x_1 = sides['-y'][j]
+                y_2, x_2 = sides['-y'][j + 1]
+
+                if y_1 == y_2 and x_1 + 1 != x_2:
+                    down_side += 1
+                elif y_1 != y_2:
+                    down_side += 1
+
+            side_count = right_side + left_side + up_side + down_side
+            result += side_count * area
+
+    return result
+
+
 if __name__ == '__main__':
     data = parse_data()
-    print(part_one_solution(data))
+    ans_1 = part_one_solution(data)
+    ans_2 = part_two_solution(data)
